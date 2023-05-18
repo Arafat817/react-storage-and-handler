1adbf254a3bb58abf58e50e75f943189d56cf0e3
Arafat817 <gr1arafat@gmail.com>
2023-05-19 02:12:27 +0600

    delete data from local storage

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import './Cosmetic.css'
-import { addToDb } from '../../utilities/fakedb';
+import { addToDb, removeFromDb } from '../../utilities/fakedb';
 
 const Cosmetic = (props) => {
     const{name,price,id}= props.cosmetic;
     const addToCart = (id) => {
         addToDb(id);
     }
-    const addToCartParameter = () => addToCart(id);
+    // const addToCartParameter = () => addToCart(id);
+    const removeFromCart = (id)=> {
+        removeFromDb(id);
+    }
     return (
         <div className='product'>
             <h2>Buy this: {name}</h2>
             <p>Only for : ${price} </p>
             <p>It has id : {id}</p>
-            <button onClick={addToCartParameter}>Add to Cart </button>
+            <button onClick={() => addToCart(id)}>Add to Cart </button>
+            <button onClick={()=> removeFromCart(id)}>Remove</button>
             {/* <button onClick={() => addToCart(id)}>Purchase </button> */}
         </div>
     );
